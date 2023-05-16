396a3388ac8cc783dc7ec96c6493f91e2becae33
German Muzquiz <german640@gmail.com>
2023-05-16 10:14:47 -0600

    CloudFormation schema

--- .config/nvim/lua/plugins/yamlcompanion.lua
+++ .config/nvim/lua/plugins/yamlcompanion.lua
@@ -28,22 +28,26 @@ return {
                 name = "Kubernetes 1.22.4",
                 uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.4-standalone-strict/all.json",
             },
-            -- {
-            --     name = "GitHub workflow",
-            --     uri = "http://json.schemastore.org/github-workflow",
-            -- },
-            -- {
-            --     name = "Kustomize files",
-            --     uri = "http://json.schemastore.org/kustomization",
-            -- },
-            -- {
-            --     name = "Helm charts",
-            --     uri = "http://json.schemastore.org/chart",
-            -- },
-            -- {
-            --     name = "Docker Compose",
-            --     uri = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json",
-            -- },
+            {
+                name = "GitHub workflow",
+                uri = "http://json.schemastore.org/github-workflow",
+            },
+            {
+                name = "Kustomize files",
+                uri = "http://json.schemastore.org/kustomization",
+            },
+            {
+                name = "Helm charts",
+                uri = "http://json.schemastore.org/chart",
+            },
+            {
+                name = "Docker Compose",
+                uri = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json",
+            },
+            {
+                name = "AWS CloudFormation",
+                uri = "https://s3.amazonaws.com/cfn-resource-specifications-us-east-1-prod/schemas/2.15.0/all-spec.json",
+            },
         },
         -- Pass any additional options that will be merged in the final LSP config
         lspconfig = {
