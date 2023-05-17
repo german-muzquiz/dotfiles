f584b8325082c0489401fa2eb59ea065c61bd680
German Muzquiz <german640@gmail.com>
2023-05-17 10:03:52 -0600

    Fixes for CloudFormation tags

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,87 +1,122 @@
-
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		-- Using protected call
-		local lsp_ok, lspconfig = pcall(require, "lspconfig")
-		if not lsp_ok then
-			return
-		end
-		local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-		if not cmp_nvim_lsp_ok then
-			return
-		end
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+        -- Using protected call
+        local lsp_ok, lspconfig = pcall(require, "lspconfig")
+        if not lsp_ok then
+            return
+        end
+        local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+        if not cmp_nvim_lsp_ok then
+            return
+        end
 
-		-- Setting up icons for diagnostics
-		local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+        -- Setting up icons for diagnostics
+        local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
 
-		-- Setting up capabilities
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+        -- Setting up capabilities
+        local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Setting up on_attach
-		local on_attach = function(_, bufnr)
+        -- Setting up on_attach
+        local on_attach = function(_, bufnr)
             local keymap = vim.keymap.set
 
-			-- Setting keymaps for lsp
-			keymap("n", "K", vim.lsp.buf.hover, { silent = true, buffer = bufnr, desc = "LSP - Displays hover information about the symbol under the cursor in a floating window. Calling the function twice will jump into the floating window." })
-			keymap("n", "gD", vim.lsp.buf.declaration, { silent = true, buffer = bufnr, desc = "LSP - Jumps to the declaration of the symbol under the cursor." })
-			-- keymapset("n", "gd", vim.lsp.buf.definition, opts)
-			keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { silent = true, buffer = bufnr, desc = "LSP - Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope." })
-			keymap("n", "gt", vim.lsp.buf.type_definition, { silent = true, buffer = bufnr, desc = "LSP - Jumps to the definition of the type of the symbol under the cursor." })
-			keymap("n", "gI", vim.lsp.buf.implementation, { silent = true, buffer = bufnr, desc = "LSP - Lists all the implementations for the symbol under the cursor in the quickfix window." })
-			keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", { silent = true, buffer = bufnr, desc = "LSP - Lists LSP references for word under the cursor, jumps to reference on <cr>" })
-			keymap("n", "dl", vim.diagnostic.open_float, { silent = true, buffer = bufnr, desc = "LSP - Show diagnostics in a floating window." })
-			keymap("n", "d]", vim.diagnostic.goto_next, { silent = true, buffer = bufnr, desc = "LSP - Move to the next diagnostic." })
-			keymap("n", "d[", vim.diagnostic.goto_prev, { silent = true, buffer = bufnr, desc = "LSP - Move to the previous diagnostic in the current buffer." })
-			keymap("n", "<leader>la", vim.lsp.buf.code_action, { silent = true, buffer = bufnr, desc = "LSP - Selects a code action available at the current cursor position." })
-			keymap("n", "<leader>lr", vim.lsp.buf.rename, { silent = true, buffer = bufnr, desc = "LSP - Renames all references to the symbol under the cursor." })
-			keymap("n", "<leader>ls", vim.lsp.buf.signature_help, { silent = true, buffer = bufnr, desc = "LSP - Displays signature information about the symbol under the cursor in a floating window. " })
-			keymap("n", "<leader>lq", vim.diagnostic.setloclist, { silent = true, buffer = bufnr, desc = "LSP - Add buffer diagnostics to the location list." })
-			keymap("n", "<leader>li", vim.cmd.LspInfo, { silent = true, buffer = bufnr, desc = "LSP - Display information about running language servers." })
-			keymap("n", "<leader>r", vim.lsp.buf.format, { silent = true, buffer = bufnr, desc = "LSP - Format current buffer." })
-		end
+            -- Setting keymaps for lsp
+            keymap("n", "K", vim.lsp.buf.hover,
+                {
+                    silent = true,
+                    buffer = bufnr,
+                    desc =
+                    "LSP - Displays hover information about the symbol under the cursor in a floating window. Calling the function twice will jump into the floating window."
+                })
+            keymap("n", "gD", vim.lsp.buf.declaration,
+                { silent = true, buffer = bufnr, desc = "LSP - Jumps to the declaration of the symbol under the cursor." })
+            -- keymapset("n", "gd", vim.lsp.buf.definition, opts)
+            keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>",
+                {
+                    silent = true,
+                    buffer = bufnr,
+                    desc =
+                    "LSP - Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope."
+                })
+            keymap("n", "gt", vim.lsp.buf.type_definition,
+                {
+                    silent = true,
+                    buffer = bufnr,
+                    desc = "LSP - Jumps to the definition of the type of the symbol under the cursor."
+                })
+            keymap("n", "gI", vim.lsp.buf.implementation,
+                {
+                    silent = true,
+                    buffer = bufnr,
+                    desc = "LSP - Lists all the implementations for the symbol under the cursor in the quickfix window."
+                })
+            keymap("n", "gr", "<cmd>Telescope lsp_references<CR>",
+                {
+                    silent = true,
+                    buffer = bufnr,
+                    desc = "LSP - Lists LSP references for word under the cursor, jumps to reference on <cr>"
+                })
+            keymap("n", "dl", vim.diagnostic.open_float,
+                { silent = true, buffer = bufnr, desc = "LSP - Show diagnostics in a floating window." })
+            keymap("n", "d]", vim.diagnostic.goto_next,
+                { silent = true, buffer = bufnr, desc = "LSP - Move to the next diagnostic." })
+            keymap("n", "d[", vim.diagnostic.goto_prev,
+                { silent = true, buffer = bufnr, desc = "LSP - Move to the previous diagnostic in the current buffer." })
+            keymap("n", "<leader>la", vim.lsp.buf.code_action,
+                {
+                    silent = true,
+                    buffer = bufnr,
+                    desc = "LSP - Selects a code action available at the current cursor position."
+                })
+            keymap("n", "<leader>lr", vim.lsp.buf.rename,
+                { silent = true, buffer = bufnr, desc = "LSP - Renames all references to the symbol under the cursor." })
+            keymap("n", "<leader>ls", vim.lsp.buf.signature_help,
+                {
+                    silent = true,
+                    buffer = bufnr,
+                    desc =
+                    "LSP - Displays signature information about the symbol under the cursor in a floating window. "
+                })
+            keymap("n", "<leader>lq", vim.diagnostic.setloclist,
+                { silent = true, buffer = bufnr, desc = "LSP - Add buffer diagnostics to the location list." })
+            keymap("n", "<leader>li", vim.cmd.LspInfo,
+                { silent = true, buffer = bufnr, desc = "LSP - Display information about running language servers." })
+            keymap("n", "<leader>r", vim.lsp.buf.format,
+                { silent = true, buffer = bufnr, desc = "LSP - Format current buffer." })
+        end
 
-		-- Setting up servers
-		local servers = { "terraformls", "pyright", "bashls", "gopls", "jsonls", "yamlls", "groovyls" }
-		for _, server in ipairs(servers) do
-			if lspconfig[server] then
-				lspconfig[server].setup({ on_attach = on_attach, capabilities = capabilities })
-			end
-		end
+        -- Setting up servers
+        require("core.ls_config")
+        for server, config in pairs(Language_server_config) do
+            if lspconfig[server] then
+                local setup = { on_attach = on_attach, capabilities = capabilities }
+                if config.cmd then
+                    setup.cmd = config.cmd
+                end
+                if config.workspace_folders then
+                    setup.workspace_folders = config.workspace_folders
+                end
+                if config.settings then
+                    setup.settings = config.settings
+                end
+                if config.filetypes then
+                    setup.filetypes = config.filetypes
+                end
+                if config.flags then
+                    setup.flags = config.flags
+                end
+                lspconfig[server].setup(setup)
+            end
+        end
 
-		-- Setting up lua server
-		lspconfig.lua_ls.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-						},
-					},
-				},
-			},
-		})
-
-        -- Setting up groovy server
-        lspconfig.groovyls.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-            filetypes = { "Jenkinsfile", "groovy" },
-            cmd = { "java", "-jar", vim.fn.stdpath("data") .. "/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar" },
-        })
-
-        vim.lsp.set_log_level("DEBUG")
-	end,
+        vim.lsp.set_log_level("INFO")
+    end,
 }
--- .config/nvim/lua/plugins/yamlcompanion.lua
+++ .config/nvim/lua/plugins/yamlcompanion.lua
@@ -1,76 +1,57 @@
-
 return {
-  'someone-stole-my-name/yaml-companion.nvim',
-  dependencies = {
-	"neovim/nvim-lspconfig",
-	"nvim-lua/plenary.nvim",
-	"nvim-telescope/telescope.nvim",
-  },
-  config = function ()
-    -- Using protected call
-    local status_ok, companion = pcall(require, 'yaml-companion')
-    if not status_ok then
-      return
-    end
+    'someone-stole-my-name/yaml-companion.nvim',
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        -- Using protected call
+        local status_ok, companion = pcall(require, 'yaml-companion')
+        if not status_ok then
+            return
+        end
 
-    require("telescope").load_extension("yaml_schema")
+        require("telescope").load_extension("yaml_schema")
+        require("core.ls_config")
 
-    companion.setup {
-        -- Built in file matchers
-        builtin_matchers = {
-            -- Detects Kubernetes files based on content
-            kubernetes = { enabled = true },
-            cloud_init = { enabled = true }
-        },
-        -- Additional schemas available in Telescope picker
-        schemas = {
-            {
-                name = "Kubernetes 1.22.4",
-                uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.4-standalone-strict/all.json",
+        companion.setup {
+            -- Built in file matchers
+            builtin_matchers = {
+                -- Detects Kubernetes files based on content
+                kubernetes = { enabled = true },
+                cloud_init = { enabled = true }
             },
-            {
-                name = "GitHub workflow",
-                uri = "http://json.schemastore.org/github-workflow",
-            },
-            {
-                name = "Kustomize files",
-                uri = "http://json.schemastore.org/kustomization",
-            },
-            {
-                name = "Helm charts",
-                uri = "http://json.schemastore.org/chart",
-            },
-            {
-                name = "Docker Compose",
-                uri = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json",
-            },
-            -- {
-            --     name = "AWS CloudFormation",
-            --     uri = "https://s3.amazonaws.com/cfn-resource-specifications-us-east-1-prod/schemas/2.15.0/all-spec.json",
-            -- },
-        },
-        -- Pass any additional options that will be merged in the final LSP config
-        lspconfig = {
-            flags = {
-                debounce_text_changes = 150,
-            },
-            settings = {
-            redhat = { telemetry = { enabled = false } },
-            yaml = {
-                validate = true,
-                format = { enable = true },
-                hover = true,
-                schemaStore = {
-                    enable = true,
-                    url = "https://www.schemastore.org/api/json/catalog.json",
+            -- Additional schemas available in Telescope picker
+            schemas = {
+                {
+                    name = "Kubernetes 1.22.4",
+                    uri =
+                    "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.4-standalone-strict/all.json",
                 },
-                schemaDownload = { enable = true },
-                schemas = {},
-                trace = { server = "debug" },
+                {
+                    name = "GitHub workflow",
+                    uri = "http://json.schemastore.org/github-workflow",
+                },
+                {
+                    name = "Kustomize files",
+                    uri = "http://json.schemastore.org/kustomization",
+                },
+                {
+                    name = "Helm charts",
+                    uri = "http://json.schemastore.org/chart",
+                },
+                {
+                    name = "Docker Compose",
+                    uri = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json",
+                },
+                -- {
+                --     name = "AWS CloudFormation",
+                --     uri = "https://s3.amazonaws.com/cfn-resource-specifications-us-east-1-prod/schemas/2.15.0/all-spec.json",
+                -- },
             },
-            },
-        },
-    }
+            -- Pass any additional options that will be merged in the final LSP config
+            lspconfig = Language_server_config["yamlls"],
+        }
     end
 }
-
