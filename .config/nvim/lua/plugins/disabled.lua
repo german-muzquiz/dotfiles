885fc97361c996cac356792baaea9baced0bd06b
German Muzquiz <german640@gmail.com>
2023-06-29 11:22:41 -0600

    Fix dt

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
 }
